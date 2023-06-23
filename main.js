84a684b8fa05b4ef31bd2504fbc2006ce22edb37
Arjunbalan <arjunbalan1994@gmail.com>
2023-06-23 11:23:54 +0530

    JS dom started

---
+++ main.js
@@ -0,0 +1,8 @@
+const userInput = document.getElementById("endorsement");
+const submitBtn = document.getElementById("submit-btn");
+const endorsementList = document.getElementById("endorsement-list");
+submitBtn.addEventListener("click", function () {
+  let valueOfInput = userInput.value;
+  console.log(valueOfInput);
+  let endoArray = [];
+});
