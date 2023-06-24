5039f1a0a24c7aa39a0d03ccae234461cb332cb8
Arjunbalan <arjunbalan1994@gmail.com>
2023-06-24 15:23:26 +0530

    html css and JS done

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const userInput = document.getElementById("endorsement");
 const submitBtn = document.getElementById("submit-btn");
 const endorsementList = document.getElementById("endorsement-list");
+let endorsementArray = [userInput.value];
+console.log(endorsementArray);
 submitBtn.addEventListener("click", function () {
   let valueOfInput = userInput.value;
-  console.log(valueOfInput);
-  let endoArray = [];
+  const liEl = document.createElement("li");
+  const divEl = document.createElement("div");
+  divEl.textContent = valueOfInput;
+  //liEl.textContent = divEl;
+  divEl.classList.add("li-div");
+  endorsementList.append(liEl);
+  liEl.append(divEl);
+  userInput.value = "";
 });
