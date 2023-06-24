e393fee5d5ffe6f011835f87abcf36fbb7274fbb
Arjunbalan <arjunbalan1994@gmail.com>
2023-06-24 15:39:21 +0530

    firebase coding started

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// firebase code
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const appSettings = {
+  databaseURL:
+    "https://realtime-database-4c002-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementInDB = ref(database, "endorsementList");
+// Webpage
 const userInput = document.getElementById("endorsement");
 const submitBtn = document.getElementById("submit-btn");
 const endorsementList = document.getElementById("endorsement-list");
@@ -8,7 +22,6 @@ submitBtn.addEventListener("click", function () {
   const liEl = document.createElement("li");
   const divEl = document.createElement("div");
   divEl.textContent = valueOfInput;
-  //liEl.textContent = divEl;
   divEl.classList.add("li-div");
   endorsementList.append(liEl);
   liEl.append(divEl);
